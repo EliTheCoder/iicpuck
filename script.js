d6b0d08a02ff6747dcd3544c4201b204388422e0
Eli Frigo <ex.games.org@gmail.com>
2020-12-22 21:48:48 -0600

    fixed formatting

--- script.js
+++ script.js
@@ -16,8 +16,8 @@ __/\\\\\\\\\\\__/\\\\\\\\\\\________/\\\\\\\\\____________/\\\\\\\\\\\\\________
 //      V V V V V V V
 
 var refreshRate = 100; // 100 - Updates position every 100 milliseconds
-var startingX = 100;   // 100 - Flag starts at X value 100
-var startingY = 100;   // 100 - Flag starts at Y value 100
+var startingX = 100; // 100 - Flag starts at X value 100
+var startingY = 100; // 100 - Flag starts at Y value 100
 
 // Don't change any of this unless you know what you're doing
 //         V V V V V V V V V V V V V V V V V V V V V v
@@ -36,8 +36,8 @@ var y = startingY;
 // The flag isn't moving anywhere yet so they're both set to nothing
 // Although, if the speed is 0, the direction could be anything, but I pick 0 because it means 'nothing'
 var velocity = {
-  speed: 0,
-  direction: 0
+	speed: 0,
+	direction: 0
 };
 
 // Defining a few variables used in the update function
@@ -47,28 +47,27 @@ var allys = [];
 
 // Function that gets updated at the refreshRate
 function update() {
-  
-  // Put the IDs of all the connected players in a variable
-  cids = IIC.getConnectedIds();
-  
-  // Put the index of the player's ID in all the IDs in a variable
-  idind = cids.indexOf(id);
-  
-  // And removing the player's own ID
-  cids.splice(idind, 1);
-  
-  // Constantly updating the variables full of all the player's coords
- allxs = [];
-  IIC.getConnectedIds().forEach(function(sid) {
-    allxs.push(IIC.getPosition(sid).x);
-    allys.push(IIC.getPosition(sid).y);
-  });
-  
-  // Constantly updating the player's x and y based on the direction they're moving
-  x += velocity.speed * Math.cos(velocity.direction);
-  y += velocity.speed * Math.sin(velocity.direction);
-  
-  // Constantly updating the player's coords w/ the x and y variables
-  IIC.setPosition(x, y);
+	// Put the IDs of all the connected players in a variable
+	cids = IIC.getConnectedIds();
+
+	// Put the index of the player's ID in all the IDs in a variable
+	idind = cids.indexOf(id);
+
+	// And removing the player's own ID
+	cids.splice(idind, 1);
+
+	// Constantly updating the variables full of all the player's coords
+	allxs = [];
+	IIC.getConnectedIds().forEach(function (sid) {
+		allxs.push(IIC.getPosition(sid).x);
+		allys.push(IIC.getPosition(sid).y);
+	});
+
+	// Constantly updating the player's x and y based on the direction they're moving
+	x += velocity.speed * Math.cos(velocity.direction);
+	y += velocity.speed * Math.sin(velocity.direction);
+
+	// Constantly updating the player's coords w/ the x and y variables
+	IIC.setPosition(x, y);
 }
 setInterval(update, refreshRate);
